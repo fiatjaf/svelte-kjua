51af11e32c3937353634dc993f82ca3064361f78
fiatjaf <fiatjaf@alhur.es>
2020-02-23 15:38:47 -0300

    v1

---
+++ QR.svelte
@@ -0,0 +1,25 @@
+<script>
+  import {onMount} from 'svelte'
+  import kjua from 'kjua'
+
+  export let value = ''
+  export let color = '#333'
+  export let size = 300
+
+  let id = parseInt(Math.random() * 100000)
+
+  onMount(() => {
+    let el = kjua({
+      rounded: 100,
+      text: value.toUpperCase(),
+      fill: color,
+      back: 'transparent',
+      size
+    })
+
+    document.getElementById('c-' + id).appendChild(el)
+  })
+</script>
+
+<div id="c-{id}"></div>
+
---
+++ README.md
@@ -0,0 +1,3 @@
+```html
+<QR value="my content" size="{300}" color="black" />
+```
